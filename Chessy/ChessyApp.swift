0e5378b25578b09221f7a8dc82c194b0147108d8
yasha424 <jakob20022404@gmail.com>
2023-07-05 20:28:24 +0300

    Initial Commit

---
+++ Chessy/ChessyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessyApp.swift
+//  Chessy
+//
+//  Created by Yasha Serhiienko on 05.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChessyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chessy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chessy
+//
+//  Created by Yasha Serhiienko on 05.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
